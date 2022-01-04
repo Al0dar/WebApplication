df198e3025de3ebc56258bda3d4f7228ffaef6d5
James <jwbonas@gmail.com>
2022-01-04 00:28:36 +0000

    Added an SSLHelper class to tidy up SSL code.

---
+++ src/springs/network/web/SSLHelper.java
@@ -0,0 +1,81 @@
+package springs.network.web;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
+
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.net.InetSocketAddress;
+import java.security.KeyStore;
+
+public class SSLHelper {
+
+    public static HttpServer CreateHTTPSServer(Integer port) throws Exception {
+
+        // implementation note : to generate the testkey.jks file, use the following from a command prompt :-
+        //
+        //      keytool -genkeypair -keyalg RSA -alias selfsigned -keystore testkey.jks -storepass password -validity 360 -keysize 2048
+        //
+        // then move the file into the 'working folder'.
+        // You can determine the 'working folder' path in java by using System.getProperty("user.dir")
+
+        HttpServer rv = null;
+
+        try {
+
+            // setup the socket address
+            InetSocketAddress address = new InetSocketAddress(port);
+
+            // initialise the HTTPS server
+            HttpsServer httpsServer = HttpsServer.create(address, 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = "password".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fis = new FileInputStream("testkey.jks");
+            ks.load(fis, password);
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+
+            rv = httpsServer;
+
+        } catch (Exception exception) {
+            System.out.println("Failed to create HTTPS server on port " + port);
+            exception.printStackTrace();
+        }
+
+        return rv;
+    }
+
+}
--- src/springs/network/web/WebServer.java
+++ src/springs/network/web/WebServer.java
@@ -25,9 +25,9 @@ public class WebServer {
 
         boolean useSSL = true;
         if (useSSL)
-            server = CreateHTTPSServer(port);
+            server = SSLHelper.CreateHTTPSServer(port);
         else
-            server = CreateHTTPServer(port);
+            server = HttpServer.create(new InetSocketAddress(port), 0);
 
         sessions = new WebSessions();
 
@@ -50,73 +50,6 @@ public class WebServer {
         return new Responder(exchange);
     }
 
-    public static HttpServer CreateHTTPSServer(Integer port) throws Exception {
 
-        // implementation note : to generate the testkey.jks file, use the following from a command prompt :-
-        //
-        //      keytool -genkeypair -keyalg RSA -alias selfsigned -keystore testkey.jks -storepass password -validity 360 -keysize 2048
-        //
-        // then move the file into the 'working folder'.
-        // You can determine the 'working folder' path in java by using System.getProperty("user.dir")
-
-        HttpServer rv = null;
-
-        try {
-            // setup the socket address
-            InetSocketAddress address = new InetSocketAddress(port);
-
-            // initialise the HTTPS server
-            HttpsServer httpsServer = HttpsServer.create(address, 0);
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-
-            // initialise the keystore
-            char[] password = "password".toCharArray();
-            KeyStore ks = KeyStore.getInstance("JKS");
-            FileInputStream fis = new FileInputStream("testkey.jks");
-            ks.load(fis, password);
-
-            // setup the key manager factory
-            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-            kmf.init(ks, password);
-
-            // setup the trust manager factory
-            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-            tmf.init(ks);
-
-            // setup the HTTPS context and parameters
-            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
-                public void configure(HttpsParameters params) {
-                    try {
-                        // initialise the SSL context
-                        SSLContext context = getSSLContext();
-                        SSLEngine engine = context.createSSLEngine();
-                        params.setNeedClientAuth(false);
-                        params.setCipherSuites(engine.getEnabledCipherSuites());
-                        params.setProtocols(engine.getEnabledProtocols());
-
-                        // Set the SSL parameters
-                        SSLParameters sslParameters = context.getSupportedSSLParameters();
-                        params.setSSLParameters(sslParameters);
-
-                    } catch (Exception ex) {
-                        System.out.println("Failed to create HTTPS port");
-                    }
-                }
-            });
-
-            rv = httpsServer;
-
-        } catch (Exception exception) {
-            System.out.println("Failed to create HTTPS server on port " + port);
-            exception.printStackTrace();
-        }
-
-        return rv;
-    }
-
-    public static HttpServer CreateHTTPServer(Integer port) throws Exception {
-        return HttpServer.create(new InetSocketAddress(port), 0);
-    }
 
 }
